7d7431a6be34125e77df6d65fb5696f9e4fd2934
Osorio Neto <osor1o@hotmail.com>
2019-06-02 12:45:31 -0300

    make migration postagens

---
+++ api/database/migrations/1559489447789_postagens_schema.js
@@ -0,0 +1,23 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class PostagensSchema extends Schema {
+  up () {
+    this.create('postagens', (table) => {
+      table.increments()
+      table.string('titulo', 120).notNullable().unique()
+      table.text('corpo').notNullable()
+      table.integer('usuarioId').unsigned()
+      table.foreign('usuarioId').references('id').inTable('users')
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('postagens')
+  }
+}
+
+module.exports = PostagensSchema
